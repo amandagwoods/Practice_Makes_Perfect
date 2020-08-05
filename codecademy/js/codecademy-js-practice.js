ae5cd0433783ed6fd9382078a2720bffd9679862
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-08-05 10:37:37 -0500

    js codecademy practice

---
+++ codecademy/js/codecademy-js-practice.js
@@ -0,0 +1,20 @@
+// Create a constant variable named entree and set it to equal to the string 'Enchiladas'.
+const entree = "Enchiladas";
+console.log(entree);
+entree = "Tacos"
+/////////
+
+// Use the += mathematical assignment operator to increase the value stored in levelUp by 5....
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+console.log(levelUp += 5);
+console.log(powerLevel-=100);
+console.log(multiplyMe *= 11);
+console.log(quarterMe /= 4);
+
+/////////////
+
